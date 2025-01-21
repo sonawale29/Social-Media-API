553fa75fb68cc5dc4d6cd1f4f9c6618786e2c42b
sonawale29 <satishsonawale29@gmail.com>
2025-01-21 15:40:59 +0530

    changes in urls.py file

--- Technostacks/Technostacks/urls.py
+++ Technostacks/Technostacks/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
-from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
